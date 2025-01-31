99e0376e7ae03267a89ef45f6c6871a31b7c24cc
Leonardo Matos <leomp120894@gmail.com>
2025-01-31 04:16:25 -0300

    Add search filter by warehouse code with customer `group`

--- template/js/custom-js/search.js
+++ template/js/custom-js/search.js
@@ -1,11 +1,28 @@
 import EcomSearch from '@ecomplus/search-engine'
+import ecomPassport from '@ecomplus/passport-client'
 
 EcomSearch.dslMiddlewares.push((dsl) => {
-  /*
+  let partnerId
+  if (ecomPassport.checkLogin()) {
+    partnerId = ecomPassport.getCustomer().group
+  }
   dsl.query.bool.filter.push({
-    terms: {
-      channel_id: [123]
+    nested: {
+      path: 'warehouses',
+      query: {
+        bool: {
+          filter: [{
+            term: {
+              'warehouses.code': partnerId || 'TESTE'
+            }
+          },
+          {
+            term: {
+              'warehouses.in_stock': true
+            }
+          }]
+        }
+      }
     }
   })
-  */
 })
